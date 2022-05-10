0d31ea5061e28886f6631c0dabb3861657927625
JackMcMillan21 <jackmcmillan1249@icloud.com>
2022-05-10 14:18:41 -0500

    responsive and added button functionality

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+
+button.addEventListener("click", () =>{
+    window.location.assign("https://docs.google.com/document/d/1mNPZUaB2tjb-QhbJ8m1VkzK3WsL3SlC_Os6xd54TKVM/edit?usp=sharing");
+});
